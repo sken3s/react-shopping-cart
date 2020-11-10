815ff3c894ad1f296e739a2e2665f0059adc12a8
Skenos Peniel <skenos3s@gmail.com>
2020-11-10 17:28:57 +0530

    added increment event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,11 +18,21 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
         {this.state.tags.length === 0 && "Please create a new tag!"}
         {this.renderTags()}
       </div>
